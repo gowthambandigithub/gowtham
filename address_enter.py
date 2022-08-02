fb459a316368894143ff1a5243471d3b14fc04d8
gowthambandigithub <109361046+gowthambandigithub@users.noreply.github.com>
2022-08-02 11:15:56 +0530

    Add files via upload

---
+++ address_enter.py
@@ -0,0 +1,8 @@
+#Enter your home or office address
+
+a=str(input("enter name: "))
+b=int(input("enter door no: "))
+c=str(input("location,area"))
+d=int(input("pincode"))
+
+print(a,b,c,d,sep="\n")
